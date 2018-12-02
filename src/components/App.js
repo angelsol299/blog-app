34d97dfce4f605bb2035cbbeeb29e133a3ba78c0
angelsol299 <angelsol299@hotmail.com>
2018-12-02 18:20:18 +0100

    styling the form

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { database } from "firebase";
 
 class App extends Component {
   constructor(props) {
@@ -7,26 +8,50 @@ class App extends Component {
       title: "",
       body: ""
     };
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onHandleSubmit = this.onHandleSubmit.bind(this);
   }
+
+  onInputChange(e) {
+    this.setState({
+      [e.target.value]: e.target.value
+    });
+  }
+
+  onHandleSubmit(e) {
+    e.preventDefault();
+    const post = {
+      title: this.state.title,
+      body: this.state.body
+    };
+    database.savePost(post);
+  }
+
   render() {
     return (
       <div className="container">
-        <form action="">
-          <input
-            type="text"
-            name="title"
-            placeholder="Title"
-            onChange={this.onInputChange}
-            ref="title"
-          />
-          <input
-            type="text"
-            name="body"
-            placeholder="Body"
-            onChange={this.onInputChange}
-            ref="body"
-          />
-          <button>Post</button>
+        <form action="" onSubmit={this.onHandleSubmit}>
+          <div className="form-group">
+            <input
+              className="form-control"
+              type="text"
+              name="title"
+              placeholder="Title"
+              onChange={this.onInputChange}
+              ref="title"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              className="form-control"
+              type="text"
+              name="body"
+              placeholder="Body"
+              onChange={this.onInputChange}
+              ref="body"
+            />
+          </div>
+          <button className="btn btn-primary">Post</button>
         </form>
       </div>
     );
