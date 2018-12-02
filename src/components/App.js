5dfea9393b4009eaf6fea0da447e1ff95a8421ae
angelsol299 <angelsol299@hotmail.com>
2018-12-02 18:07:02 +0100

    form

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,33 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      body: ""
+    };
+  }
   render() {
     return (
-      <div className="App">
-        <h1>holas</h1>
+      <div className="container">
+        <form action="">
+          <input
+            type="text"
+            name="title"
+            placeholder="Title"
+            onChange={this.onInputChange}
+            ref="title"
+          />
+          <input
+            type="text"
+            name="body"
+            placeholder="Body"
+            onChange={this.onInputChange}
+            ref="body"
+          />
+          <button>Post</button>
+        </form>
       </div>
     );
   }
