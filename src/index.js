05c41018c996dd4ba9eca907549f9bddbe6dfd6b
angelsol299 <angelsol299@hotmail.com>
2018-12-03 20:53:34 +0100

    passing reducers to redux store

--- src/index.js
+++ src/index.js
@@ -5,12 +5,20 @@ import * as serviceWorker from "./serviceWorker";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers";
 
 //create redux store -> reducers -> actions | applyMiddleware()
 
-const store = createStore();
+const store = createStore(rootReducer, composeWithDevTools());
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//provide store to react
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
