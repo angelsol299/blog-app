f366cd1a881488999272d42ceaaa6e69023fb7fc
angelsol299 <angelsol299@hotmail.com>
2018-12-03 19:48:19 +0100

    renering posts

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,38 @@
 import React, { Component } from "react";
 import { database } from "../firebase";
+import _ from "lodash";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       title: "",
-      body: ""
+      body: "",
+      posts: {}
     };
+    // binding
     this.onInputChange = this.onInputChange.bind(this);
     this.onHandleSubmit = this.onHandleSubmit.bind(this);
+  }
+
+  // lifeCycle method
+  componentDidMount() {
+    database.on("value", snapshot => {
+      this.setState({
+        posts: snapshot.val()
+      });
+    });
+  }
+  //render post from firebase
+  renderPosts() {
+    return _.map(this.state.posts, (post, key) => {
+      return (
+        <div key={key}>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </div>
+      );
+    });
   }
 
   onInputChange(e) {
@@ -53,6 +76,8 @@ class App extends Component {
           </div>
           <button className="btn btn-primary">Post</button>
         </form>
+        <br />
+        {this.renderPosts()}
       </div>
     );
   }
